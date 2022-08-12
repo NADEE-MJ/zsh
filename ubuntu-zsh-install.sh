48cfb2a4a5138f7f5533d850c5e8fd910170b183
Nadeem Maida <84353378+NADEE-MJ@users.noreply.github.com>
2022-08-12 11:57:56 -0700

    Update ubuntu-zsh-install.sh

--- ubuntu-zsh-install.sh
+++ ubuntu-zsh-install.sh
@@ -4,13 +4,18 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install locales python3.10 python-is-python3 python3-pip vim wget git curl zsh cargo ripgrep dnsutils net-tools tig fzf bat -y
 pip install psutil
 
-#? exa and zoxide setup
-cargo install exa
-cargo install zoxide --locked
-
 sudo dpkg-reconfigure --frontend noninterative locales
 sudo locale-gen --purge "en_US.UTF-8"
 sudo update-locale "LANG=en_US.UTF-8"
+
+#? exa and zoxide setup
+cargo install exa
+cargo install zoxide
+
+#? bat install
+mkdir ~/packages/
+curl -L https://github.com/sharkdp/bat/releases/download/v0.21.0/bat-musl_0.21.0_amd64.deb > ~/packages/bat.deb
+sudo dpkg -i ~/packages/bat.deb
 
 #? personal zsh files install
 if ! (rg "zsh" ~/.bashrc) then
