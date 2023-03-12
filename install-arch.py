e0c06580be8a3ec09f13fff31499ba2c17dd10f1
NADEE-MJ <nadeem.maida@gmail.com>
2023-03-12 16:46:24 -0700

    updated install

--- install-arch.py
+++ install-arch.py
@@ -2,24 +2,25 @@
 
 import os
 
-os.system('sudo pacman -Syyu --noconfirm')
-os.system('sudo pacman -S vim wget git curl micro python python-pip zsh ripgrep dnsutils net-tools tig fzf bat exa zoxide --noconfirm')
-os.system('sudo pacman -S cowsay figlet lolcat --noconfirm')
-os.system('pip install psutil')
+os.system("sudo pacman -Syyu --noconfirm")
+os.system(
+    "sudo pacman -S vim wget git curl micro python python-pip zsh ripgrep dnsutils net-tools tig fzf bat exa zoxide python-psutil --noconfirm"
+)
+os.system("sudo pacman -S cowsay figlet lolcat --noconfirm")
 
-HOME = os.path.expanduser('~')
-with open(f'{HOME}/.bashrc', 'r') as file:
-        content = file.read()
-        if 'zsh' not in content:
-            os.system('echo "zsh" >> ~/.bashrc')
+HOME = os.path.expanduser("~")
+with open(f"{HOME}/.bashrc", "r") as file:
+    content = file.read()
+    if "zsh" not in content:
+        os.system('echo "zsh" >> ~/.bashrc')
 
-os.system('sudo rm -r ~/.config/zsh/')
-os.system('mkdir -p ~/.config/zsh/')
-os.system('git clone --depth 1 https://github.com/NADEE-MJ/zsh.git ~/.config/zsh')
+os.system("sudo rm -r ~/.config/zsh/")
+os.system("mkdir -p ~/.config/zsh/")
+os.system("git clone --depth 1 https://github.com/NADEE-MJ/zsh.git ~/.config/zsh")
 
-os.system('rm ~/.zshrc')
-os.system('ln -s ~/.config/zsh/.zshrc ~/.zshrc')
-os.system('rm ~/.p10k.zsh')
-os.system('ln -s ~/.config/zsh/.p10k.zsh ~/.p10k.zsh')
+os.system("rm ~/.zshrc")
+os.system("ln -s ~/.config/zsh/.zshrc ~/.zshrc")
+os.system("rm ~/.p10k.zsh")
+os.system("ln -s ~/.config/zsh/.p10k.zsh ~/.p10k.zsh")
 
-os.system('exec zsh')
+os.system("exec zsh")
