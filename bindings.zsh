a5421bc080e02578210df89ee2f860b98c775a74
NADEE-MJ <nadeem.maida@gmail.com>
2023-07-11 14:44:58 -0700

    updates

--- bindings.zsh
+++ bindings.zsh
@@ -2,6 +2,8 @@
 bindkey "^[[1;5C" forward-word # CTRL+RIGHT
 bindkey "^[[1;5D" backward-word # CTRL+LEFT
 bindkey "^[[3~" delete-char # DELETE
+bindkey "forward-kill-word" kill-word # DELETE
+bindkey "backward-kill-word" backward-kill-word # DELETE
 
 # substring search, type in what to search for and substring will only display commands with that in it
 zle -N history-substring-search-up
